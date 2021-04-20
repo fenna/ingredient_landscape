18eb77c0449774349dde6dad31ed79a74f198338
Fenna Feenstra <flen@mac.com>
2021-04-20 09:48:57 +0200

    framework for webportal (image part)

--- DataModel.py
+++ DataModel.py
@@ -5,17 +5,16 @@ import sqlite3
 import numpy as np
 import os
 from wordcloud import WordCloud
-
 from io import BytesIO
 import base64
 
-#fig, (ax1, ax2) = plt.subplots(2)
 keyword1 = 'tomato'
-keyword2 = 'italian'
+keyword2 = 'soup'
+
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10,8))
 
 
-SQL = f'''SELECT R.ID, R.tag_value as recipe_name, N.tag_value 
+SQL = f'''SELECT R.ID, R.tag_value as recipe_name, N.tag_value as ner_name
          FROM recipe R 
          JOIN recipe_ner RN ON R.ID = RN.recipe_id
          JOIN ner N ON RN.ner_id = N.ID
@@ -23,65 +22,140 @@ SQL = f'''SELECT R.ID, R.tag_value as recipe_name, N.tag_value
          AND R.tag_value LIKE '%{keyword2}%'
          ;'''
 
-print(SQL)
 
 currentDirectory = os.path.dirname(os.path.abspath(__file__))
 DB = currentDirectory +'/database/recipe.db'
 
+
+class RecipeQuery:
+
+    def __init___(self):
+
+        self.sql = f"""SELECT R.ID, R.tag_value as recipe_name, N.tag_value as ner_name
+                        FROM recipe R 
+                        JOIN recipe_ner RN ON R.ID = RN.recipe_id
+                        JOIN ner N ON RN.ner_id = N.ID"""
+
+    def __repr__(self):
+        print(self.sql)
+    
+
+    def queryOnRecipe(self, searched_recipe, exact_search):
+        """ create the query based on the form
+        Inputs: keywords for searaching in recipes, boolean to indicate type of search
+        Output: query
+        """
+        query = self.sql
+        if exact_search == '1':
+            query += f"""WHERE R.tag_value LIKE '%{searched_recipe}%';"""
+        else:
+            query += f"""WHERE R.tag_value LIKE '%{searched_recipe.split(' ')[0]}%'"""
+            if len(searched_recipe.split(' ')) > 1:
+                for i in searched_recipe.split(' ')[1:]:
+                    query += f""" AND R.tag_value LIKE '%{i}%'"""
+            query += """;"""
+        return query
+
+
+    def queryOnRecipeNer(searched_recipe, searched_ner, exact_search):
+        sql = self.queryOnRecipe(searched_ner, exact_search)
+    
+        #Inputs: keywords for searaching in recipes, boolean to indicate type of search
+        #Output: list of tuples as result, grouped dataframe on recipes 
+    
+        # generate first query to find recipe ID match criteria
+    
+        query1 = sql + f''' INTERSECT
+                SELECT RN.recipe_id
+                FROM recipe_ner RN
+                JOIN ner N ON RN.ner_id = N.ID
+                WHERE N.tag_value LIKE '%{searched_ner}%'
+                '''
+
+        # generate second query
+        query2 = f"""SELECT R.ID, R.tag_value, N.tag_value 
+                FROM recipe R 
+                JOIN recipe_ner RN ON R.ID = RN.recipe_id
+                JOIN ner N ON RN.ner_id = N.ID
+                WHERE R.ID IN ({query1});"""
+        return query2
+
+
+
+
+class RecipeIngredients:
+    def __init__(self, df):
+        self.df = df
+
+    @property
+    def group(self):
+        try:
+            grouped = self.df.groupby(['ID', 'recipe_name'])['ner_name'].apply(list).reset_index()
+        except:
+            grouped = pd.DataFrame()
+        return grouped
+
+    def __repr__(self):
+        print(self.group)
+
+
+
 class DataModel:
 
-    
-    def bar_plot(self, filename):
-        """
-        Create a barplot from a csv file, use numpy to read a bytes stream
-        :param filename: from form file upload
-        :return: image stream to be placed in <img> tag
-        """
-        #read from database
-        conn = sqlite3.connect(DB)
-        df = pd.read_sql(SQL, conn)
+    def __init__(self, sql=SQL, db=DB):
+ 
+        self.df = pd.read_sql(sql, sqlite3.connect(db))
+        self.df_recipes = self.df.drop_duplicates(subset = ['ID'])
+        self.total = len(self.df_recipes)
+        self.grouped = RecipeIngredients(self.df).group
+        
 
-        #get recipes
-        df_recipes = df.drop_duplicates(subset = ['ID'])
-        total = len(df_recipes)
+    def get_origin(self, n=10):
+        """ method that adds a column origin to the dataframe and selects the 10 largest into a new dataframe"""
+        self.df_recipes['origin'] = self.df_recipes['recipe_name'].apply(org.get_origin)
+        self.df_recipes = self.df_recipes[self.df_recipes.origin != '']
+        dfnlargest = self.df_recipes.origin.value_counts().nlargest(n)
+        return dfnlargest
 
-        #get origin
-        df_recipes['origin'] = df_recipes['recipe_name'].apply(org.get_origin)
-        df_recipes = df_recipes[df_recipes.origin != '']
-        dfnlargest = df_recipes.origin.value_counts().nlargest(10)
 
+    def get_count(self):
+        """ method that gets the counts of the ingredients"""
+        freq = self.df.iloc[:,2:3].value_counts().nlargest(20)
+        ingredients = list(list(zip(*freq.index))[0])
+        counts = list(freq)
+        return ingredients, counts
+
+
+    def create_fig(self, filename):
+        """ method that plots the data """
+
+        #create origin pie plot
+        dfnlargest = self.get_origin()
         ax3.pie(dfnlargest, labels = dfnlargest.index, autopct='%1.1f%%', textprops={'fontsize': 8})
         ax3.set_title('top 10 locations')
 
-        # plot ingredient occurence
-        freq = df.iloc[:,2:3].value_counts().nlargest(20)
-        ingredients = list(list(zip(*freq.index))[0])
-        counts = list(freq)
-    
+        #create count per ingredient plot
+        ingredients, counts = self.get_count()
         ax1.bar(ingredients, counts, color='red', alpha = 0.7)
         ax1.set_ylabel('ingredient occurence', fontsize = 8)
         ax1.set_xticks(range(0,20))
         ax1.set_xticklabels(ingredients, rotation= 90, fontsize = 8)
         
-        #plot number of ingredients distribution
-        recipe_count = df.groupby(by="ID").count()
-        recipe_count = recipe_count.iloc[:,1:2].reset_index().rename(columns = {'tag_value':'count'})
-        rc = recipe_count.drop_duplicates(subset = 'ID')
-        
-        ax2.hist(rc['count'], color ='red', bins=50, alpha = 0.7)
+        #create number of ingredients distribution plot
+        rc = self.df.groupby(by="ID").count()
+        ax2.hist(rc.ner_name, color ='red', bins=50, alpha = 0.7)
         ax2.set_xlabel("# ingredients", fontsize = 8)
         ax2.set_ylabel('counts', fontsize = 8)
-
-        text = " ".join(i for i in df.tag_value)
+        
+        #create wordcloud plot
+        text = " ".join(i for i in self.df.ner_name)
         wordcloud = WordCloud(stopwords=org.stopwords, background_color="white").generate(text)
         ax4.imshow(wordcloud)
         ax4.axis('off')
 
-
-        fig.suptitle(f'{total} {keyword1} {keyword2} recipes')
+        #main title
+        fig.suptitle(f'{self.total} {keyword1} {keyword2} recipes')
         fig.tight_layout()
-
-
 
         return self.create_website_plot(fig)
 
